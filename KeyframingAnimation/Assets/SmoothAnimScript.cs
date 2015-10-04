2910c1e700667444cd6ac26fdc75f9038d6fa7bc
Ned Tugent <mendesry@gatech.edu>
2015-10-03 20:30:31 -0400

    Beginning Scripting  + Animator Variables
    Nothing big, starting to write the script to compliment the animation
    controller; variables + skeleton methods.
    Some parameters set up in the SmoothAnimController state machine.

---
+++ KeyframingAnimation/Assets/SmoothAnimScript.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SmoothAnimScript : MonoBehaviour {
+
+	private Animator animator;
+	private CapsuleCollider capCollider;
+	private SphereCollider sphereCollider;
+	private Rigidbody rigidBody;
+	private AnimatorStateInfo currentState;
+
+	//TODO: Set up states here using nameToHash
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator> ();
+//		capCollider = GetComponent<CapsuleCollider> ();
+//		sphereCollider = GetComponent<SphereCollider> ();
+		rigidBody = GetComponent<Rigidbody> ();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		currentState = animator.GetCurrentAnimatorStateInfo(0); //Get the current state
+	
+	}
+
+	void FixedUpdate() {
+		float horizontalAxis = Input.GetAxis ("Horizontal");
+		float verticalAxis = Input.GetAxis ("Vertical");
+		animator.SetFloat ("VertSpeed", verticalAxis);
+		animator.SetFloat ("HorizSpeed", verticalAxis);
+
+	}
+
+	void CheckState() {
+		string thisState = "";
+//		if (currentState.nameHash == idle)
+		Debug.Log ("State = " + thisState);
+	}
+}
