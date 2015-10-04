a99c9bb1dd6bd956551c3fbb68aca65e4616259d
Neal Kaviratna <Nkaviratna@gatech.edu>
2015-10-04 02:15:47 -0400

    Added Hook punch

--- KeyframingAnimation/Assets/Scripts/MeleeInput.cs
+++ KeyframingAnimation/Assets/Scripts/MeleeInput.cs
@@ -8,12 +8,18 @@ public class MeleeInput : MonoBehaviour {
 	List<Attack> attackString = new List<Attack>();
 
 	public float inputCooldown;
+	private float inputCooldownlength;
+
+	// TODO: turn on the hitboxes in a better way
+	public Collider hitbox1;
+	public Collider hitbox2;
 
 	public Animator player;
 
 	// Use this for initialization
 	void Start () {
-		inputCooldown = 0.5f;
+		inputCooldownlength = 0.5f;
+		inputCooldown = inputCooldownlength;
 	}
 	
 	// Update is called once per frame
@@ -21,9 +27,9 @@ public class MeleeInput : MonoBehaviour {
 		// Take in user input with a cooldown for time between presses and add them to attack string
 		if (inputCooldown < 0) {
 			if (Input.GetMouseButtonDown(0)) {
-				attackString.Add(Attack.jab);  inputCooldown = 0.5f; }
+				attackString.Add(Attack.jab);  inputCooldown = inputCooldownlength; }
 			else if (Input.GetMouseButtonDown(1)) {
-				attackString.Add(Attack.uppercut); inputCooldown = 0.5f; }
+				attackString.Add(Attack.uppercut); inputCooldown = inputCooldownlength; }
 //			else if (Input.GetButtonDown(";"))
 //				attackString.Add(Attack.kick);
 		}
@@ -37,7 +43,14 @@ public class MeleeInput : MonoBehaviour {
 			attackString.Remove(next);
 			Debug.Log(next);
 		}
-		else if(player.GetCurrentAnimatorStateInfo(1).IsTag("attack"))
+		else if(player.GetCurrentAnimatorStateInfo(1).IsTag("attack")) {
 			player.SetInteger("NextAttack", 0);
+			hitbox1.enabled = true;
+			hitbox2.enabled = true;
+		}
+		else {
+			hitbox1.enabled = false;
+			hitbox2.enabled = false;
+		}
 	}
 }
