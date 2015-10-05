eb20d9f548435e9c05011da0df51ad4794833069
Robert <RobertBorowicz7@gmail.com>
2015-10-04 20:51:10 -0400

    Eating
    Camera Stuff

---
+++ KeyframingAnimation/Assets/Scripts/FollowerCamera_2.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowerCamera_2 : MonoBehaviour {
+
+    public GameObject target;
+    private float dist_back = 1f;
+    private float dist_up = 1f;
+    public float smoothing = 2;
+    Vector3 followTarget;
+
+    void Start()
+    {}
+
+    void LateUpdate()
+    {
+        followTarget = target.transform.position + Vector3.up * dist_up - target.transform.forward * dist_back;
+        transform.position = Vector3.Lerp(transform.position, followTarget, Time.deltaTime * smoothing);
+        transform.LookAt(target.transform);
+    }
+}
