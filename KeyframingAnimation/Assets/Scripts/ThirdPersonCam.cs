13c861707fa1e288bb7b2eb3bc4ced7cbc877208
Robert <RobertBorowicz7@gmail.com>
2015-10-04 21:26:05 -0400

    Third Person Scene Added

---
+++ KeyframingAnimation/Assets/Scripts/ThirdPersonCam.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThirdPersonCam : MonoBehaviour
+{
+
+    public Transform target;
+    Transform self;
+
+
+    void Start()
+    {
+        self = GetComponent<Transform>();
+    }
+
+    void LateUpdate()
+    {
+        self.position = target.position;
+    }
+}
