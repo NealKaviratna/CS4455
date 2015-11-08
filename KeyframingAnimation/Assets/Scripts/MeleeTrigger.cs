7fcab1c1445c7009176e74feaf095d5d5c704f55
Robert <RobertBorowicz7@gmail.com>
2015-11-08 17:02:50 -0500

    Combat System Updated
    Can now punch/kick/hadouken enemies and kill them. Attacks occur as part
    of the animation so they are invoked once per attack. Punches and kicks
    are set to 10 damage currently and hadouken is 50. Enemy health is set
    to 100

---
+++ KeyframingAnimation/Assets/Scripts/MeleeTrigger.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MeleeTrigger : MonoBehaviour {
+
+    List<GameObject> nearbyEnemies = new List<GameObject>();
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void Melee()
+    {
+        Debug.Log("Melee");
+        Vector3 myPos = transform.position;
+        for (int i = 0; i < nearbyEnemies.Count; i++)
+        {
+            Vector3 ePos = nearbyEnemies[i].transform.position;
+            Vector3 diff = ePos - myPos;
+            Debug.Log(Vector3.Dot(diff, transform.forward));
+            float dist = Vector3.Dot(diff, transform.forward);
+            if (dist <= 1.25f && dist > 0f) {
+                nearbyEnemies[i].GetComponent<Enemy>().takeHit(10);
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider coll)
+    {
+        Debug.Log("Detected");
+        if (coll.GetComponent<Collider>().tag == "Enemy")
+        {
+            nearbyEnemies.Add(coll.gameObject);
+        }
+    }
+
+    void OnTriggerExit(Collider coll)
+    {
+        if (coll.GetComponent<Collider>().tag == "Enemy")
+        {
+            nearbyEnemies.Add(coll.gameObject);
+        }
+    }
+}
