38ea33d5b40878cc36da0977ea4f4aef5809219c
Robert <RobertBorowicz7@gmail.com>
2015-11-15 17:26:44 -0500

    Main Menu Updates

---
+++ KeyframingAnimation/Assets/MenuCameraMovement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuCameraMovement : MonoBehaviour {
+
+    public Transform currentMount;
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = Vector3.Lerp(transform.position, currentMount.position, speed);
+        transform.rotation = Quaternion.Slerp(transform.rotation, currentMount.rotation, speed);
+	}
+
+    void SetMount(Transform newMount)
+    {
+        currentMount = newMount;
+    }
+}
