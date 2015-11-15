5b3cfb56ca421c4b5bfacd59d63c0852cea40871
Robert <RobertBorowicz7@gmail.com>
2015-11-15 17:47:38 -0500

    Volume Slider Working in Options
    Volume slider works, but it's kinda unintuitive and currently works just
    for the current scene. Will need to change later. Just testing for now.

---
+++ KeyframingAnimation/Assets/GUIHandler.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GUIHandler : MonoBehaviour {
+
+    public AudioSource source;
+    public Slider volumeSlider;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void SetVolume()
+    {
+        source.volume = volumeSlider.value;
+    }
+}
