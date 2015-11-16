2bce4b490ef1d5ec4e2889f600c9d440ff28a080
Robert <RobertBorowicz7@gmail.com>
2015-11-15 23:35:46 -0500

    Credits Scrolling

---
+++ KeyframingAnimation/Assets/CreditsScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreditsScript : MonoBehaviour {
+
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = transform.position + Vector3.up * Time.deltaTime * speed;
+	}
+}
