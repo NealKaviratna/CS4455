c3ecfbced8ca53d5b7a32470ec1d597297d49fab
Robert <RobertBorowicz7@gmail.com>
2015-11-17 23:37:18 -0500

    CreditsCredtitsCredits
    i need ryans face

--- KeyframingAnimation/Assets/Scripts/GameScripts/CreditsScript.cs
+++ KeyframingAnimation/Assets/Scripts/GameScripts/CreditsScript.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class CreditsScript : MonoBehaviour {
 
     public float speed;
+    public FadeScreen fade;
+    private bool exit = false;
 
 	// Use this for initialization
 	void Start () {
@@ -13,5 +15,10 @@ public class CreditsScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         transform.position = transform.position + Vector3.up * Time.deltaTime * speed;
+        if (transform.position.y > 15.5f && !exit)
+        {
+            exit = true;
+            fade.SwitchScene(0);
+        }
 	}
 }
