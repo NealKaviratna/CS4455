5850748b31dae129b113646cf0744c4509e20413
Robert Borowicz <robertborowicz7@gmail.com>
2015-12-03 20:16:23 -0500

    Score Manager Script Started

---
+++ KeyframingAnimation/Assets/Scripts/GameScripts/UI Scripts/ScoreManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreManager : MonoBehaviour {
+
+	private int score = 0;
+	private float gameTimer;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameTimer += Time.deltaTime;
+		//gameTimer = Mathf.Round (gameTimer * 100f) / 100;
+		Debug.Log (gameTimer.ToString("F2"));
+	}
+
+	public void addScore(int value) {
+		score += value;
+	}
+
+	public int getScore() {
+		return score;
+	}
+}
