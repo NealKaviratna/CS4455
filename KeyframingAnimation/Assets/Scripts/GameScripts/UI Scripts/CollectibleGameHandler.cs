58b1d613a73c602bd5dea1679a785d2db87ab44a
Robert <RobertBorowicz7@gmail.com>
2015-12-04 22:50:14 -0500

    Collectibles Menu and Static Scripts for Handling Their Collection

---
+++ KeyframingAnimation/Assets/Scripts/GameScripts/UI Scripts/CollectibleGameHandler.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollectibleGameHandler : MonoBehaviour {
+
+    public float rotationRate = 40f;
+    private bool taken;
+    public int collectible_number;
+
+    public GameObject player;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        transform.RotateAround(transform.position, Vector3.up, rotationRate * Time.deltaTime);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            if (!taken)
+            {
+                taken = true;
+                CollectItem();
+            }
+        }
+    }
+
+    void CollectItem()
+    {
+        CollectiblesStatic.collectibles[collectible_number] = true;
+        Destroy(gameObject);
+    }
+}
+
