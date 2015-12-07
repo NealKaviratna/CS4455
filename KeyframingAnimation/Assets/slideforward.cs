783e1284ddbb69e223ab8c1a5ef143a564bfcfa4
Neal Kaviratna <Nkaviratna@gatech.edu>
2015-12-07 09:13:01 -0500

    Air Slide fix

---
+++ KeyframingAnimation/Assets/slideforward.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class slideforward : StateMachineBehaviour {
+
+	private CharacterController cc;
+	private float timer = 0.0f;
+
+	 //OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+		this.timer = 0.1f;
+		cc = GameObject.FindGameObjectWithTag("Player").GetComponentInChildren<CharacterController>();
+	}
+
+	 //OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+	override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+		timer -= Time.deltaTime;
+		Debug.Log(timer);
+		if (this.timer < 0) {
+			this.cc.SimpleMove(cc.transform.forward);
+		}
+	}
+
+	// OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+	//override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	//
+	//}
+
+	// OnStateMove is called right after Animator.OnAnimatorMove(). Code that processes and affects root motion should be implemented here
+	//override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	//
+	//}
+
+	// OnStateIK is called right after Animator.OnAnimatorIK(). Code that sets up animation IK (inverse kinematics) should be implemented here.
+	//override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	//
+	//}
+}
