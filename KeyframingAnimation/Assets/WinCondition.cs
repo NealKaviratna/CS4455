6874928be63e0b5c199fc2b674a24a7fce406e20
Robert Borowicz <robertborowicz7@gmail.com>
2015-12-07 07:38:37 -0500

    Menus and Skyboxes

--- KeyframingAnimation/Assets/WinCondition.cs
+++ KeyframingAnimation/Assets/WinCondition.cs
@@ -19,7 +19,7 @@ public class WinCondition : MonoBehaviour {
     {
         if (coll.gameObject.tag == "Player")
         {
-            wgh.SignalWin();
+            if (wgh) wgh.SignalWin();
         }
     }
 }
---
+++ KeyframingAnimation/Assets/win.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class win : MonoBehaviour {
+
+	//public FadeScreen fs;
+	public GameObject menu;
+
+	// Use this for initialization
+	void Start () {
+		menu.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Win() {
+		menu.SetActive (true);
+	}
+
+}
