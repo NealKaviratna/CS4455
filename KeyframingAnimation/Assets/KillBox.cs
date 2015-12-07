6fe2c630f86f91dfd8004b27fb9bbbbaec1961c0
Robert Borowicz <robertborowicz7@gmail.com>
2015-12-07 04:22:23 -0500

    Level Updates

---
+++ KeyframingAnimation/Assets/KillBox.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KillBox : MonoBehaviour {
+
+	public PlayerHealth ph;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider coll) {
+		if (coll.gameObject.tag == "Player") {
+			ph.die();
+		}
+	}
+}
